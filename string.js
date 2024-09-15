5fe16da13901850cc4c592fba15b73fdec1ef0d7
Iishhii <ishielmishma24@gmail.com>
2024-09-15 21:39:53 +0800

    Inital commit

---
+++ string.js
@@ -0,0 +1,13 @@
+let favActorFirstName = "Heeyu";
+let favActorLastName = "Lee";
+let fullName = favActorFirstName + "" + favActorLastName;
+console.log(fullName);
+
+let uppercase = "my favorite actor name ise heeyu";
+console.log(uppercase);
+
+let lowercase = "my favorite color is BLACK";
+console.log(lowercase);
+
+let message = "my favorite actor is HEEYU_LEE; his best show is Loved for Ever";
+console.log(message);
